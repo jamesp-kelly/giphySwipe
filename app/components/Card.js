4630e7fd16f3f92cb0a23bb53f177daa006457d3
James Kelly <jk_jameskelly@outlook.com>
2017-06-06 09:19:48 -0400

    Swipe and spring-back animations

--- app/components/Card.js
+++ app/components/Card.js
@@ -4,17 +4,120 @@ import {
   View,
   Image,
   StyleSheet, 
-  TouchableHighlight
+  TouchableHighlight,
+  Animated,
+  PanResponder
 } from 'react-native';
 
-const Card = ({ imageUri, onImagePress }) => {
-  return (
-      <View style={styles.card}>
+class Card extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flingRotateAngle: '45deg'
+    };
+  }
+
+  componentWillMount() {
+    this.positionAnimatedValue = new Animated.ValueXY();
+    this.rotationAnimatedValue = new Animated.Value(0);
+
+    this._value = {x: 0, y: 0};
+    this.positionAnimatedValue.addListener((value) => this._value = value);
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.positionAnimatedValue.setOffset({
+          x: this._value.x,
+          y: this._value.y
+        });
+        this.positionAnimatedValue.setValue({x: 0, y: 0});
+      },
+      onPanResponderMove: Animated.event([
+        null, { dx: this.positionAnimatedValue.x, dy: this.positionAnimatedValue.y }
+      ]),
+      onPanResponderRelease: (e, gestureState) => {
+        this.positionAnimatedValue.flattenOffset();
+
+        if (gestureState.dx > 150) {
+          this.flingCardRight();
+        } else if (gestureState.dx < -150) {
+          this.flingCardLeft();
+        } else {
+          this.returnCardToCenter();
+        }
+      }
+    })
+  }
+
+  flingCardLeft() {
+    this.setState({
+      flingRotateAngle: '-45deg'
+    });
+
+    Animated.parallel([
+      Animated.timing(this.positionAnimatedValue.x, {
+        toValue: -500,
+        duration: 400
+      }),
+      Animated.timing(this.rotationAnimatedValue, {
+        toValue: 1,
+        duration: 400
+      })
+    ]).start();
+  }
+
+  flingCardRight() {
+    this.setState({
+      flingRotateAngle: '45deg'
+    });
+
+    Animated.parallel([
+      Animated.timing(this.positionAnimatedValue.x, {
+        toValue: 500,
+        duration: 400
+      }),
+      Animated.timing(this.rotationAnimatedValue, {
+        toValue: 1,
+        duration: 400
+      })
+    ]).start();
+  }
+
+  returnCardToCenter() {
+    Animated.spring(this.positionAnimatedValue, {
+      toValue: 0,
+      duraction: 600
+    }).start();
+  }
+
+  render() {
+    const animatedStyle = {
+      transform: this.positionAnimatedValue.getTranslateTransform()
+    };
+
+    const interpolateRotation = this.rotationAnimatedValue.interpolate({
+      inputRange: [0,1],
+      outputRange: ['0deg', this.state.flingRotateAngle]
+    });
+    
+    animatedStyle.transform = [
+      ...animatedStyle.transform,
+      {rotate: interpolateRotation}
+    ];
+
+    const { imageUri, onImagePress } = this.props;
+
+    return (
+      <Animated.View style={[styles.card, animatedStyle]} {...this.panResponder.panHandlers}>
         <TouchableHighlight style={styles.box} onPress={onImagePress}>
           <Image style={styles.image} source={{uri: imageUri}} />
         </TouchableHighlight>
-      </View>
-  );
+      </Animated.View>
+    );
+  }
 }
 
 Card.PropTypes = {
